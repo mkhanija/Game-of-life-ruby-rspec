d583127a04361a3a22dcf417dabb0bb33e3d9a74
Manan Khanija <manan.khanija@aux.go-jek.com>
2018-07-02 16:43:24 +0700

    Added more input files for demo

--- lib/conway_game_of_life_redo.rb
+++ lib/conway_game_of_life_redo.rb
@@ -2,12 +2,12 @@ require_relative "universe"
 
 module GameOfLife
   
-  universe = GameOfLife::Universe.new("glider")
+  universe = GameOfLife::Universe.new("pulsar")
   
-  for i in 0..15
+  for i in 0..100
     system "clear"
     universe.print_universe
-    sleep(1)
+    sleep(0.2)
     universe.process_universe
   end
 

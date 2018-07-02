c5d8a6519980bc58ee0ca69e9d417091153c1a46
Manan Khanija <manan.khanija@aux.go-jek.com>
2018-07-02 14:02:43 +0700

    Added initializing universe spec

--- lib/universe.rb
+++ lib/universe.rb
@@ -1,5 +1,24 @@
 module GameOfLife
   class Universe
+    def initialize(height, width, grid)
+      @height = height
+      @width = width
+      @grid = grid 
+    end
+
+    def height()
+      @height
+    end
+
+    def width()
+      @width
+    end
+
+    def grid() 
+      @grid
+    end
     
+    
+
   end
 end
--- spec/universe_spec.rb
+++ spec/universe_spec.rb
@@ -1,7 +1,14 @@
 require "./lib/universe"
 
 describe "Universe" do
-  it "can pass this test" do
-    expect(0).to eq(0)
+  it "can create a universe with given values for the height, width and the grid" do
+    
+    grid_array = Array.new(5){Array.new(5, 0)}
+
+    universe = GameOfLife::Universe.new(5, 5, grid_array)
+    
+    expect(universe.height).to eq(5)
+    expect(universe.width).to eq(5)
+    expect(universe.grid).to eq(grid_array)
   end
 end
